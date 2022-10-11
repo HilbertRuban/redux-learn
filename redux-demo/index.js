5aa0c823c03253a63d97e79cc783b9fc8a951159
Hilbert <hilbertruban53@gmail.com>
2022-10-12 01:42:00 +0530

    initial commit

---
+++ redux-demo/index.js
@@ -0,0 +1,121 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+
+const reduxLogger = require("redux-logger");
+const logger = reduxLogger.createLogger();
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICEREAM_RESTOCKED = "ICEREAM_RESTOCKED";
+
+function oderCake(qty = 1) {
+  return {
+    type: CAKE_ORDERED,
+    payload: qty,
+  };
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
+}
+
+function orderIceCream(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+
+function restockIceCream(qty = 1) {
+  return {
+    type: ICEREAM_RESTOCKED,
+    payload: qty,
+  };
+}
+
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCream: 20,
+// };
+
+const initialCakeState = {
+  numOfCakes: 10,
+};
+
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
+
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - action.payload,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    case ICEREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload,
+      };
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+console.log("initial state", store.getState());
+
+const unsubscribe = store.subscribe(() => {});
+
+// store.dispatch(oderCake());
+// store.dispatch(oderCake());
+// store.dispatch(oderCake());
+// store.dispatch(restockCake(3));
+
+const actions = bindActionCreators(
+  { oderCake, restockCake, orderIceCream, restockIceCream },
+  store.dispatch
+);
+actions.oderCake();
+actions.oderCake();
+actions.oderCake();
+actions.restockCake(3);
+actions.orderIceCream();
+actions.orderIceCream();
+actions.restockIceCream(2);
+unsubscribe();
